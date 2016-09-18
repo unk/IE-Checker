26e6d8e9848726945e138b410075803f5af15522
Unknown GrotesQ <unknown@grotesq.com>
2016-09-19 00:47:55 +0900

    - 1.0.1 - Edge 에 대한 탐색 추가

--- ie-checker-min.js
+++ ie-checker-min.js
@@ -1,2 +1,2 @@
-/*! IE-Checker v1.0.0 | (c) 2007-2015 The GrotesQ | https://github.com/Unk/IE-Checker */
-!function(){var e=navigator.userAgent.toLowerCase();if(-1!=e.indexOf("msie")||-1!=e.indexOf("trident")){var a=11;e=/msie ([0-9]{1,}[\.0-9]{0,})/.exec(e),e&&(a=parseInt(e[1]));var i="";i+=" is-ie",i+=" ie"+a;for(var t=a+1;11>=t;t++)i+=" lt-ie"+t;document.getElementsByTagName("html")[0].className+=i}}();
+/*! IE-Checker v1.0.1 | (c) 2007-2016 The GrotesQ | https://github.com/Unk/IE-Checker */
+!function(){function c(a){var c="";c+=" is-ie",c+=" ie"+a;for(var d=a+1;d<=b;d++)c+=" lt-ie"+d;document.getElementsByTagName("html")[0].className+=c}var a=navigator.userAgent.toLowerCase(),b=14;if(/edge\/([0-9]{1,}[\.0-9]{0,})/.test(a)){if(a=/edge\/([0-9]{1,}[\.0-9]{0,})/.exec(a))var d=parseInt(a[1]);c(d)}else if(a.indexOf("msie")!=-1||a.indexOf("trident")!=-1){var d=11;if(a=/msie ([0-9]{1,}[\.0-9]{0,})/.exec(a))var d=parseInt(a[1]);c(d)}}();
--- ie-checker.js
+++ ie-checker.js
@@ -1,30 +1,44 @@
 /*!
- * IE-Checker v1.0.0
+ * IE-Checker v1.0.1
  * https://github.com/Unk/IE-Checker
  *
  * Author : Naram Kim ( a.k.a. Unknown )
  * Email : unknown@grotesq.com
  *
- * Copyright 2007-2015 The GrotesQ
+ * Copyright 2007-2016 The GrotesQ
  * Released under the MIT license
  *
- * Date: 2015-05-13 14:47
+ * Date: 2016-09-19 00:46
  */
 ;( function() {
 	var ua = navigator.userAgent.toLowerCase();
-	if( ua.indexOf( 'msie' ) != -1 || ua.indexOf( 'trident' ) != -1 ) {
-		var version = 11;
-		ua = /msie ([0-9]{1,}[\.0-9]{0,})/.exec( ua );
-		if( ua )
-		{
-			version = parseInt( ua[ 1 ] );
-		}
+	var latest = 14;
+
+	function addClass( version ) {
 		var classNames = '';
 		classNames += ' is-ie';
 		classNames += ' ie' + version;
-		for( var i = version + 1; i <= 11; i++ ) {
+		for( var i = version + 1; i <= latest; i++ ) {
 			classNames +=  ' lt-ie' + i;
 		}
 		document.getElementsByTagName( 'html' )[ 0 ].className += classNames;
+	}
+
+	if( /edge\/([0-9]{1,}[\.0-9]{0,})/.test( ua ) ) {
+		ua = /edge\/([0-9]{1,}[\.0-9]{0,})/.exec( ua );
+		if( ua ) {
+			var version = parseInt( ua[ 1 ] );
+		}
+
+		addClass( version );
+	}
+	else if( ua.indexOf( 'msie' ) != -1 || ua.indexOf( 'trident' ) != -1 ) {
+		var version = 11;
+		ua = /msie ([0-9]{1,}[\.0-9]{0,})/.exec( ua );
+		if( ua ) {
+			var version = parseInt( ua[ 1 ] );
+		}
+		
+		addClass( version );
 	}
 } )();
